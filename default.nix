86f27e2e80eef797a517e7323c539e32b8c8f482
Emmanouil Kampitakis <emmanouil.kampitakis@ionos.com>
2022-11-25 18:41:01 +0100

    First version of the package

---
+++ default.nix
@@ -0,0 +1,45 @@
+{ stdenv, dpkg }:
+let
+  version = "2.1.21-2";
+  rpath = stdenv.lib.makeLibraryPath [
+
+  ] + ":${stdenv.cc.cc.lib}/lib64";
+
+  src = 
+	if stdenv.hostPlatform.system = "x86_64-linux" then
+	  fetchurl {
+		urls = [ "https://app.drivestrike.com/static/apt/pool/main/d/drivestrike/drivestrike_${version}_amd64.deb" ];
+		sha256 = "059m3x387kjz4j3c0sbz3wdbh2dn0dxq1rq8vhdbf5m3bswc3i4d";
+	  }
+	else
+	  throw "Drivestrike is not supported on ${stdenv.hostPlatform.system}";
+in stdenv.mkDerivation {
+  pname = "drivestrike";
+  inherit version;
+
+  system = "x86_64-linux";
+
+  inherit src;
+
+  nativeBuildInputs = [ glib ];
+
+  buildInputs = [ dpkg ];
+
+  dontUpack = true;
+
+  installPhase = ''
+	mkdir -p $out
+	dpkg -x $src $out
+	cp -av $out/usr/* $out
+	rm -rf $out/usr
+
+	chmod -R g-w $out
+  '';
+
+  meta = with stdenv.lib; {
+	description = "Drivestrike for Linux";
+	homepage = "https://drivestrike.com";
+	license = licenses.unfree;
+	platforms = [ "x86_64-linux" ];
+  }
+}
